13efdf0bafa6df8e288ec609a6f5a94847e2a0da
mohammadfaghedi <mohammad.faghedi@hotmail.com>
2023-10-01 12:27:41 +0200

    Your commit message here

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const target = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: target.offsetTop - 20,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Add interactivity or additional functionality here
